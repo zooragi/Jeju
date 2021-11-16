03e969e2c09e77f9599e3f0769171ed626fc3ea7
eldehd9898 <eldehd9898@gmail.com>
2021-11-16 09:34:52 +0900

    feat : Complete setting

--- src/main/java/com/jdh/jeju/JejuApplication.java
+++ src/main/java/com/jdh/jeju/JejuApplication.java
@@ -3,14 +3,10 @@ package com.jdh.jeju;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication(scanBasePackages = {"com.jdh.jeju"})
-@EnableWebMvc
 public class JejuApplication {
 
 	public static void main(String[] args) {
