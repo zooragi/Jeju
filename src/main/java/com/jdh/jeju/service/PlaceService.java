1cb120e4a1d3f887945cf79c7a50d308106e432b
eldehd9898 <eldehd9898@gmail.com>
2021-11-16 21:46:03 +0900

    feat : Add PlaceService

---
+++ src/main/java/com/jdh/jeju/service/PlaceService.java
@@ -0,0 +1,16 @@
+package com.jdh.jeju.service;
+
+import com.jdh.jeju.dao.PlaceDao;
+import com.jdh.jeju.domain.Place;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlaceService {
+
+    @Autowired
+    private PlaceDao placeDao;
+
+}
